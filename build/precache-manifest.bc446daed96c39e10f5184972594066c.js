3a827c20e698a3dbfd03e834477e52431e76b80e
Diavolochief <erick_alonso56@hotmail.com>
2022-09-20 15:38:09 -0500

    [push] pagina

---
+++ build/precache-manifest.bc446daed96c39e10f5184972594066c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa306d02f5fcde1dbd45",
+    "url": "/landings/landpark/static/css/main.261fa113.chunk.css"
+  },
+  {
+    "revision": "fa306d02f5fcde1dbd45",
+    "url": "/landings/landpark/static/js/main.fa306d02.chunk.js"
+  },
+  {
+    "revision": "c71a9b3d26031c0fb609",
+    "url": "/landings/landpark/static/js/runtime~main.c71a9b3d.js"
+  },
+  {
+    "revision": "f9488c3c5bb5f7e78c55",
+    "url": "/landings/landpark/static/css/2.6c1d2474.chunk.css"
+  },
+  {
+    "revision": "f9488c3c5bb5f7e78c55",
+    "url": "/landings/landpark/static/js/2.f9488c3c.chunk.js"
+  },
+  {
+    "revision": "0e6af5412d6687e977202479951d6a96",
+    "url": "/landings/landpark/index.html"
+  }
+];
