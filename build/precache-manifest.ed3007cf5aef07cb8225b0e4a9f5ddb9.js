c6684314c974b6a63c6af1def7bb66bdb69b5ad5
Diavolochief <erick_alonso56@hotmail.com>
2022-09-21 14:56:08 -0500

    ll

---
+++ build/precache-manifest.ed3007cf5aef07cb8225b0e4a9f5ddb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "65ee0684a0372a5ebe2d",
+    "url": "/landp/static/css/main.261fa113.chunk.css"
+  },
+  {
+    "revision": "65ee0684a0372a5ebe2d",
+    "url": "/landp/static/js/main.65ee0684.chunk.js"
+  },
+  {
+    "revision": "538e0b09ae915dfd583b",
+    "url": "/landp/static/js/runtime~main.538e0b09.js"
+  },
+  {
+    "revision": "f9488c3c5bb5f7e78c55",
+    "url": "/landp/static/css/2.6c1d2474.chunk.css"
+  },
+  {
+    "revision": "f9488c3c5bb5f7e78c55",
+    "url": "/landp/static/js/2.f9488c3c.chunk.js"
+  },
+  {
+    "revision": "bf84d1174862eced2c89333ba81d1ef9",
+    "url": "/landp/index.html"
+  }
+];
